c052506efb4dfc84de258074289887d9f0c1b3f3
skipina <paintballsean@gmail.com>
2024-05-11 18:48:54 +0300

    added another change to quadD

--- quadD/piscine/quadD.go
+++ quadD/piscine/quadD.go
@@ -4,15 +4,12 @@ import "github.com/01-edu/z01"
 
 func QuadD(x, y int) {
 	if x > 0 && y > 0 {
-
 		for row := 1; row <= y; row++ {
 			for col := 1; col <= x; col++ {
-				if row == 1 && col == 1 {
+				if (row == 1 && col == 1) || (row == y && col == 1) {
 					z01.PrintRune('A')
-				} else if (row == 1 && col == x) || (row == y && col == 1) {
+				} else if (row == 1 && col == x) || (row == y && col == x) {
 					z01.PrintRune('C')
-				} else if row == y && col == x {
-					z01.PrintRune('B')
 				} else if (row == 1 || row == y) && (col > 1 && col < x) {
 					z01.PrintRune('B')
 				} else if (col == 1 || col == x) && (row > 1 && row < y) {
