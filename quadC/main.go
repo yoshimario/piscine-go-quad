12bf82527be7b815015edf75ffa89e8d4598539c
czunigar <caro@carozuniga.com>
2024-05-11 18:25:21 +0300

    folders and files

---
+++ quadC/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "piscine/quadC/piscine"
+
+func main() {
+	piscine.QuadC(5, 3)
+}
---
+++ quadC/piscine/quadC.go
@@ -0,0 +1,27 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadC(x, y int) {
+	if x > 0 && y > 0 {
+
+		for row := 1; row <= y; row++ {
+			for col := 1; col <= x; col++ {
+				if row == 1 && col == 1 {
+					z01.PrintRune('A')
+				} else if (row == 1 && col == x) || (row == y && col == 1) {
+					z01.PrintRune('C')
+				} else if row == y && col == x {
+					z01.PrintRune('C')
+				} else if (row == 1 || row == y) && (col > 1 && col < x) {
+					z01.PrintRune('B')
+				} else if (col == 1 || col == x) && (row > 1 && row < y) {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
