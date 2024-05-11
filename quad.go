4421072a2f1f801ffafdab4029c4866f4a0f5d88
skipina <paintballsean@gmail.com>
2024-05-11 20:00:06 +0300

    added final function to quad.go

--- quad.go
+++ quad.go
@@ -91,3 +91,29 @@ func QuadD(x, y int) {
 		}
 	}
 }
+
+func QuadE(x, y int) {
+	if x > 0 && y > 0 {
+
+		for row := 1; row <= y; row++ {
+			for col := 1; col <= x; col++ {
+				if row == 1 && col == 1 {
+					z01.PrintRune('A')
+				} else if row == 1 && col == x {
+					z01.PrintRune('C')
+				} else if row == y && col == 1 {
+					z01.PrintRune('C')
+				} else if row == y && col == x {
+					z01.PrintRune('A')
+				} else if (row == 1 || row == y) && (col > 1 && col < x) {
+					z01.PrintRune('B')
+				} else if (col == 1 || col == x) && (row > 1 && row < y) {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
