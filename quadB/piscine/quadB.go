5ad367766ce00b6891db1ff14563e5dc5deb1be7
czunigar <caro@carozuniga.com>
2024-05-11 18:04:55 +0300

    quadB

--- quadB/piscine/quadB.go
+++ quadB/piscine/quadB.go
@@ -1,5 +1,27 @@
 package piscine
 
-func QuadB(x, y int) {
+import "github.com/01-edu/z01"
 
+func QuadB(x, y int) {
+	if x > 0 && y > 0 {
+
+		for row := 1; row <= y; row++ {
+			for col := 1; col <= x; col++ {
+				if row == 1 && col == 1 {
+					z01.PrintRune('/')
+				} else if (row == 1 && col == x) || (row == y && col == 1) {
+					z01.PrintRune('\\')
+				} else if row == y && col == x {
+					z01.PrintRune('/')
+				} else if (row == 1 || row == y) && (col > 1 && col < x) {
+					z01.PrintRune('*')
+				} else if (col == 1 || col == x) && (row > 1 && row < y) {
+					z01.PrintRune('*')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	}
 }
