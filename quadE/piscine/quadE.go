e91f57d8cba55775a64e7ebddb7d76f3719223e9
czunigar <caro@carozuniga.com>
2024-05-11 19:03:47 +0300

    quadE

--- quadE/piscine/quadE.go
+++ quadE/piscine/quadE.go
@@ -2,14 +2,14 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func QuadD(x, y int) {
+func QuadE(x, y int) {
 	if x > 0 && y > 0 {
 
 		for row := 1; row <= y; row++ {
 			for col := 1; col <= x; col++ {
-				if (row == 1 && col == 1) || (row == y && col == 1) {
+				if (row == 1 && col == 1) || (row == y && col == x) {
 					z01.PrintRune('A')
-				} else if (row == 1 && col == x) || (row == y && col == x) {
+				} else if (row == 1 && col == x) || (row == y && col == 1) {
 					z01.PrintRune('C')
 				} else if (row == 1 || row == y) && (col > 1 && col < x) {
 					z01.PrintRune('B')
